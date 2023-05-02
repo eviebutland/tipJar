8d6801cfe3b2b060714680c61ec80e6042813514
EvieButland <evie.butland@hectare.farm>
2023-05-02 07:06:18 +0100

    first commit

---
+++ app/entry.client.tsx
@@ -0,0 +1,21 @@
+/**
+ * By default, Remix will handle hydrating your app on the client for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.client
+ */
+
+import { RemixBrowser } from "@remix-run/react";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>
+  );
+});
+
+// Hydrate root is a react feature that allows you to attach react to existing HTML that was rendered by the server.
+// Hydrate expects the content to be the exact same as the server-rendered content. Any mismatches are bugs.
