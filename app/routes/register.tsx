e5fdfc2d0eafda82c33f4ac756e5a1b20a358a95
EvieButland <evie.butland@hectare.farm>
2023-05-08 10:01:40 +0100

    trying to get form to work

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -3,12 +3,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { ActionArgs } from "@remix-run/node";
 import { db } from "~/utils/db.server";
 import { Form } from "@remix-run/react";
-
+import { useActionData } from "@remix-run/react";
+import { badRequest } from "~/utils/request.server";
 // By exporting the action - we do not need to have a submit function linked to the form. It is all handled here
 export const action = async ({ request }: ActionArgs) => {
   try {
     const form = await request.formData();
 
+    console.log(form);
     const formData = {
       name: form.get("name"),
       email: form.get("email"),
@@ -28,9 +30,21 @@ export const action = async ({ request }: ActionArgs) => {
         ...formData,
       },
     });
+
     console.log(newUser);
+    badRequest({
+      fieldErrors: [],
+      fields: formData,
+      formError: null,
+    });
+
     return newUser;
   } catch (error) {
+    badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: "Form not submitted correctly.",
+    });
     console.log(error);
   }
 };
@@ -39,22 +53,27 @@ const Register = () => {
   function handleViewPassword() {
     console.log("change input type password to text");
   }
+
+  const actionData = useActionData<typeof action>();
   return (
     <div className="centered-container">
       <h1>Register</h1>
       <Form method="post" action="/register" className="w-1/2">
         <label>
-          Name: <input type="text" name="name" />
+          Name:
+          <input defaultValue={actionData?.name} type="text" name="name" />
         </label>
 
         <label>
-          Email: <input type="text" name="email" />
+          Email:{" "}
+          <input type="text" defaultValue={actionData?.email} name="email" />
         </label>
 
         <label>
           Password:
           <div className="flex items-center">
             <input
+              defaultValue={actionData?.password}
               type="password"
               name="password"
               className="flex-1 mr-2"
@@ -69,15 +88,21 @@ const Register = () => {
         </label>
 
         <label>
-          Bio: <textarea name="bio" />
+          Bio: <textarea name="bio" defaultValue={actionData?.bio} />
         </label>
 
         <label>
-          Role: <input type="text" name="role" />
+          Role:{" "}
+          <input type="text" name="role" defaultValue={actionData?.role} />
         </label>
 
         <label>
-          Profile picture: <input type="text" name="profilePicture" />
+          Profile picture:{" "}
+          <input
+            type="text"
+            name="profilePicture"
+            defaultValue={actionData?.profilePicture}
+          />
         </label>
 
         <hr />
@@ -85,13 +110,27 @@ const Register = () => {
         <section>
           <h2>Payment information</h2>
           <label>
-            Card no: <input type="number" name="cardNo" />
+            Card no:
+            <input
+              type="number"
+              name="cardNo"
+              defaultValue={actionData?.payment.cardNo}
+            />
           </label>
           <label>
-            Sort code: <input type="number" name="sortCode" />
+            Sort code:{" "}
+            <input
+              type="number"
+              name="sortCode"
+              defaultValue={actionData?.payment.sortCode}
+            />
           </label>
         </section>
-
+        {actionData?.fieldErrors?.name ? (
+          <p className="form-validation-error" id="name-error" role="alert">
+            {actionData.fieldErrors.name}
+          </p>
+        ) : null}
         <button type="submit" className="priority">
           Register
         </button>
